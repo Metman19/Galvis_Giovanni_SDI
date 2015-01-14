3eb5622d0e54b97ce00c9be624ea14a80e10efc2
Gio <ggalvis@1sale.com>
2015-01-14 00:36:18 -0500

    Values and Variables assignment
    Values and Variables assignment

---
+++ Variables - Defining/script.js
@@ -0,0 +1,19 @@
+// Defining Variables
+
+var age =  38; //Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Giovanni";
+var lastName = "Galvis";
+
+var phrase = 'I don\'t \n know';
+
+// Arrays
+var muppetNames = ["Giovanni", "Galvis", 0, 4, false, true, 8]; //lets me see 4
+muppetNames[1] = "Rizzo"; //shows me Rizzo
+muppetNames[2] = "Fozzy";
+
+console.log(muppetNames[2]);
+
+document.write(muppetNames[1]);
