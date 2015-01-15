3c4fdd99f4b95c3fd4c768164772effe9ac804c7
Gio <ggalvis@1sale.com>
2015-01-14 19:28:39 -0500

    Prompted user for name
    Used an alert to say Hello

--- GoTo Training 2/js/gotoTraining2.js
+++ GoTo Training 2/js/gotoTraining2.js
@@ -4,4 +4,16 @@
  * GoTo Training Day #2
  * 1/14/2015
  */
-alert("Testing to see if the JS File is attached :D");
+//alert("Testing to see if the JS File is attached :D");
+
+//Create an age calculator
+
+//Ask the user their name
+var name = prompt("Please type in your name");
+
+//console.log out the name
+console.log(name);
+
+//Welcome the user with
+alert("Welcome " + name + "! Let's figure out how you are.");
+
