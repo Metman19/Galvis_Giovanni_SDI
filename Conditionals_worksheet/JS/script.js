849ff2bb7eb6aedb973163613a29e4520bc62c4f
Gio <ggalvis@1sale.com>
2015-01-20 17:20:04 -0500

    Started working on Conditionals
    Started working on Conditionals by creating the folders and pages
    necessary for this assignment

---
+++ Conditionals_worksheet/JS/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Giovanni Galvis
+ * 1/20/15
+ * Conditionals_worksheet
+ */
+
+
+//Stuff your face I
+
+var compWeight;
+
+if(compWeight >= 250){
+    console.log("The competitor qualifies for the heavyweight division.");
+} else {
+    console.log("The competitor needs to gain some weight!");
+}
