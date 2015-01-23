3c5d6a53afb6a9e4b5ab83944b0c2eb63c6dec8a
Gio <ggalvis@1sale.com>
2015-01-23 01:09:18 -0500

    Finished second conditional
    I finished the 2nd conditional for male/female burned calories.

--- Galvis_Giovanni_Conditionals_Assignment/JS/script.js
+++ Galvis_Giovanni_Conditionals_Assignment/JS/script.js
@@ -59,7 +59,40 @@ if(isNaN(age) || age ===""){
     console.log(age);
 }
 var miles = prompt("how many miles did you run today?");
-console.log("You ran " + miles + " miles today!");
 if(isNaN(miles)){
     miles = prompt("Please enter a numerical value, how many miles did you run today");
+} else if(miles >= 10) {
+    console.log("Wow, You ran " + miles + " miles today!");
+} else if (miles <10){
+    console.log("You ran " + miles + " miles today! Nice!");
 }
+var weight = prompt("How much do you weight?");
+if(isNaN(weight) || weight ===""){
+    weight = prompt("Please enter a numerical value, how old are you?");
+} else {
+    console.log("You weigh " + weight + "lbs.");
+}
+var heartRate = prompt("What is your heart rate?");
+if(isNaN(heartRate) || heartRate ===""){
+    heartRate = prompt("Please enter a numerical value, what is your heart rate?");
+} else {
+    console.log("You heart rate is " + heartRate + " beats per minute");
+}
+var time = prompt("How long was your workout?", 10);
+if(isNaN(time) || time ===""){
+    time = prompt("Please enter a numerical value, How long was your workout?");
+} else {
+    console.log("Your workout lasted " + time + " minutes");
+}
+var femaleCal = ((age * 0.074) - (weight * 0.05741) + (heartRate * 0.4472) - 20.4022) * time / 4.184;
+
+var maleCal = ((age * 0.2017) - (weight * 0.09036) + (heartRate * 0.6309) - 55.0969) * time / 4.184;
+femaleCal = femaleCal.toFixed(2);
+maleCal = maleCal.toFixed(2);
+if(maleOrFemale === "male"){
+    alert("You burned " + maleCal + " calories.");
+} else if (maleOrFemale === "female"){
+    alert("You burned " + femaleCal + " calories.");
+}
+//I finally did it, I made a calories burned calculation.
+//It worked I entered same values for male and female had different results for each.
