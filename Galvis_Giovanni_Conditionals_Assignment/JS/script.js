6bbb67f69b947862a1baab268adb1d3ed8836f47
Gio <ggalvis@1sale.com>
2015-01-23 02:12:51 -0500

    Added in 1st Flowchart
    Added in 1st Flowchart for first conditional statement

--- Galvis_Giovanni_Conditionals_Assignment/JS/script.js
+++ Galvis_Giovanni_Conditionals_Assignment/JS/script.js
@@ -8,7 +8,7 @@
 Here is the first conditional task, It is for new home owners and they want to find out the square footage
 for their master bedroom.
 */
-/*alert("Congratulations on your new property, please provide some information.");
+alert("Congratulations on your new property, please provide some information.");
 var height=(prompt("Hello, what is the height of the room?"));
 if(height==="" || isNaN(height)){
 //This means the user typed in nothing or Not a Number
@@ -38,7 +38,7 @@ if(squareFootage >= 250){
     console.log("This is a nice cozy room");
     console.log("This room is worth $" + squareFootPrice);
 
-} */
+}
 /*
 I typed in the values for the variables of height, length and squareFootageCost and found out the size of the room and value.
  */
