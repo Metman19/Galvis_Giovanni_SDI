ed20020250a5f8e6439aee765825d366cf63e7d4
Gio <ggalvis@1sale.com>
2015-01-24 19:05:38 -0500

    Finishing up conditionals worksheet
    Finishing up conditionals worksheet

--- Conditionals_worksheet/JS/script.js
+++ Conditionals_worksheet/JS/script.js
@@ -6,18 +6,19 @@
 
 
 //Stuff your face I
-
+/*
 var compWeight = prompt("How much do you weigh?");
-
+console.log("You weigh " + compWeight+"lbs.");
 if(compWeight >= 250){
     console.log("The competitor qualifies for the heavyweight division.");
 } else {
     console.log("The competitor needs to gain some weight!");
 }
-
+*/
 //Group 1: Expressions with Conditionals
 
 //Celsius to Fahrenheit converter
+/*
 var centDeg;
 var degFahren;
 var question = prompt("Do you want to know the temperature in C or F?");
@@ -34,9 +35,9 @@ if(question == "F"){
     centDeg =  5/9 * (centDeg-32);
     console.log("The temperature is " + centDeg + " degrees Celsius.");
 }
-
+*/
 //Last Chance for Gas!
-var mpg = 25;
+/*var mpg = 25;
 var gasCap = 16;
 var milesDriven;
 milesDriven = Number(prompt("How many miles have you driven?"));
@@ -50,4 +51,33 @@ if(gauge >= 200){
     gauge = gauge / mpg;
     console.log("You only have " + gauge + " gallons of gas in your tank, better get gas now while you can!");
 }
+*/
+// Group 2 Grade Letter Calculator
+
+//first variable will be the grade in percentage number
+/*
+var gradeScore = prompt("what score did you receive in this course?");
+ /*if (gradeScore >=95) {
+    console.log("You have a " + gradeScore + "%, which means you have earned a(n) A+ in the class!");
+}
+else if (gradeScore >=90){
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) A in the class!");
+} else if (gradeScore >=85){
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) B+ in the class!");
+} else if (gradeScore >=80){
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) B in the class!");
+}  else if (gradeScore >=76){
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) C+ in the class!");
+} else if (gradeScore >=73){
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) C in the class!");
+} else if (gradeScore >=70){
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) D in the class!");
+} else {
+    console.log("You have a "+gradeScore+"%, which means you have earned a(n) F in the class!");
+}*/
+
+//Group 3 Movie Ticket Price
+var ticketPrice = 12;
+var age;
+var time;
 
