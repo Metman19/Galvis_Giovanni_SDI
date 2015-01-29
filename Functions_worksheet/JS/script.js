e35438ce6e976c95099c7a5bdfdfd52f3af940d8
Gio <ggalvis@1sale.com>
2015-01-29 01:40:42 -0500

    Finished first problem of circumference.
    Finished first problem of circumference.

--- Functions_worksheet/JS/script.js
+++ Functions_worksheet/JS/script.js
@@ -2,4 +2,25 @@
  * Created by Giovanni Galvis on 1/29/15.
  * Functions Worksheet
  */
-//This is the first problem on how to calculate the area of a rectangle.
+//Calculate the area of a rectangle.
+/* var width =5;
+var height =6;
+var area = calculateArea(width,height);
+console.log("The Area of the Rectangle is " + area);
+
+function calculateArea(w,h){
+    return w*h;
+} */
+//example problem
+
+// First Problem - Calculate the circumference of a circle.
+
+var radius = 30;
+var pie = 3.14159;
+var circumference = calcCir(radius,pie);
+console.log("The circumference of the circle is " + circumference);
+
+function calcCir(r,p){
+    return r*p*2;
+}
+//The parameters that go into the function calculate correctly each time.
