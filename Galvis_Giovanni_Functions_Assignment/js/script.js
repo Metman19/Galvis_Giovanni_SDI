bb5f187c6f8b08a0e18e3ab6af9281e02278c12a
Gio <ggalvis@1sale.com>
2015-01-29 21:25:36 -0500

    Calculate the volume of the pyramid
    Have the user input all the data we need to run the formula.

--- Galvis_Giovanni_Functions_Assignment/js/script.js
+++ Galvis_Giovanni_Functions_Assignment/js/script.js
@@ -4,4 +4,18 @@
  * Functions Assignment
  */
 //testing to make sure script is connected to html.
-alert("Hello Functions");
+//alert("Hello Functions");
+
+//Created first function
+// let's find out the volume of a pyramid, the formula for this is volume = 1/3*h*l*w.
+
+var height = prompt("What is the height of the pyramid?");
+console.log("The height of the pyramid is " + height + " feet");
+var width = prompt("What is the width of the pyramid?");
+console.log("The width of the pyramid is " + width + " feet");
+var length = prompt("What is the length of this pyramid?");
+console.log("The length of this pyramid is " + length + " feet");
+
+function pyramidVol(height,width,length){
+    //Now we calculate the volume of the pyramid
+}
