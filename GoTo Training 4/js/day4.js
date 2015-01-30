be7d00bae459cb3d6eee9de76157d2745c9a8d38
Gio <ggalvis@1sale.com>
2015-01-29 19:06:00 -0500

    Created min prompt and validated
    Created min prompt and validated

--- GoTo Training 4/js/day4.js
+++ GoTo Training 4/js/day4.js
@@ -5,3 +5,29 @@
  */
 
 //alert("Hello World");
+
+//Create a file that gives us a random number between
+//two numbers
+
+//Ask the user for a min number
+var min = prompt("Let's find a random number between two numbers.\nPlease enter a minimum number:");
+
+//Validate that user prompt
+/*
+Old way of doing it was
+if(min==="" || isNaN(min){
+    //prompt the user
+    min = prompt("Please do not leave blank and only use numbers.");
+
+}
+ */
+
+//isNaN() - is Not a Number
+console.log(isNaN("cat"));
+console.log(isNaN(7));
+
+//New way of validating is with While loops
+while(min==="" || isNaN(min)){
+    //re-prompt the user
+    min = prompt("Please do not leave blank and only use numbers!\mPlease type in min value:");
+}
